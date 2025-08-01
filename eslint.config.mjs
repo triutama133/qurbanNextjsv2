599658a0791ecf0f98dc0bc928e78bc6c584544e
Trix Utama <hello@MacBook-Air-hello.local>
2025-08-02 05:27:29 +0700

    eslint int

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,5 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+export default [
+  {
+    extends: ["next", "next/core-web-vitals"],
+  },
+];
